fea51c1591e96fbfac05a5c3bb1ef6d2d56f388e
acwga <3074027753@qq.com>
2026-02-28 20:39:54 +0800

    web上传文件模块

---
+++ app_file_uploader.py
@@ -0,0 +1,28 @@
+"""
+基于Streamlit完成web网页上传服务
+"""
+import streamlit as st
+
+# 添加网页标题
+st.title('知识库更新服务')
+
+# 添加文件上传组件
+uploader_file = st.file_uploader(
+    label='请添加你要上传的txt文件',
+    type=['txt'],
+    accept_multiple_files=False
+)
+
+# 如果用户上传了文件，显示文件信息
+if uploader_file:
+    file_name = uploader_file.name
+    file_type = uploader_file.type
+    file_size = uploader_file.size / 1024
+
+    st.subheader(f'文件名: {file_name}')
+    st.write(f'文件类型: {file_type}')
+    st.write(f'文件大小: {file_size:.2f} KB')
+
+    text = uploader_file.getvalue().decode('utf-8')
+    st.write('文件内容:')
+    st.write(text)
