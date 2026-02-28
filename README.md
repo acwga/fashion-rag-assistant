403ec01eaf3938c6cc21b4f47674e78754cdb8e9
acwga <3074027753@qq.com>
2026-02-28 22:30:42 +0800

    知识库搭建2

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # fashion-rag-assistant
 基于RAG的服装商品智能客服
+加载知识库web网页指令: & D:/Anaconda3/envs/data_analysis/python.exe -m streamlit run app_file_uploader.py
--- app_file_uploader.py
+++ app_file_uploader.py
@@ -2,6 +2,8 @@
 基于Streamlit完成web网页上传服务
 """
 import streamlit as st
+from knowledge_base import KnowledgeBaseService
+import time
 
 # 添加网页标题
 st.title('知识库更新服务')
@@ -12,6 +14,10 @@ uploader_file = st.file_uploader(
     type=['txt'],
     accept_multiple_files=False
 )
+
+# 初始化知识库服务对象，并将其存储在Session State中，以便在不同的交互中保持状态
+if 'service' not in st.session_state:
+    st.session_state['service'] = KnowledgeBaseService()
 
 # 如果用户上传了文件，显示文件信息
 if uploader_file:
@@ -24,5 +30,9 @@ if uploader_file:
     st.write(f'文件大小: {file_size:.2f} KB')
 
     text = uploader_file.getvalue().decode('utf-8')
-    st.write('文件内容:')
-    st.write(text)
+    
+    # 显示加载动画，并调用知识库服务的upload_by_str方法将文本上传到知识库中
+    with st.spinner('载入知识库中...'):
+        time.sleep(2)
+        result = st.session_state['service'].upload_by_str(text, file_name)
+        st.write(result)
